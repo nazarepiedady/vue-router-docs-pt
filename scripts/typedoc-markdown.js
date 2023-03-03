39c3e34ba35bd8c9457269c2beeb9979f391327b
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-03-03 12:17:51 +0000

    chore: update `scripts/typedoc-markdown.js`

--- scripts/typedoc-markdown.js
+++ scripts/typedoc-markdown.js
@@ -13,7 +13,7 @@ const DEFAULT_OPTIONS = {
   cleanOutputDir: true,
   excludeInternal: true,
   readme: 'none',
-  out: path.resolve(__dirname, '../api'),
+  out: path.resolve(__dirname, './api'),
   entryDocument: 'index.md',
   hideBreadcrumbs: false,
   hideInPageTOC: true,
@@ -38,6 +38,9 @@ exports.createTypeDocApp = function createTypeDocApp(config = {}) {
   /** @type {'build' | 'serve'} */
   let targetMode = 'build'
 
+  const slugify = s => s.replaceAll(' ', '-')
+  // encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-'))
+
   app.renderer.on(
     PageEvent.END,
     /**
@@ -45,17 +48,53 @@ exports.createTypeDocApp = function createTypeDocApp(config = {}) {
      * @param {import('typedoc/dist/lib/output/events').PageEvent} page
      */
     page => {
-      console.log(page.url)
-      if (page.url === 'index.md') {
-        // strip off the first line because it's the title without the #
-        page.contents = page.contents.slice(page.contents.indexOf('\n') + 1)
+      if (page.url !== 'index.md' && page.contents) {
+        page.contents = prependYAML(page.contents, {
+          // TODO: figure out a way to point to the source files?
+          editLink: false,
+        })
       }
-      page.contents = prependYAML(page.contents, {
-        sidebar: 'auto',
-        // TODO: figure out a way to point to the source files?
-        editLinks: false,
-        sidebarDepth: 3,
-      })
+
+      // avoid duplicated id titles
+      if (page.contents) {
+        const lines = page.contents.split('\n')
+        const titleStack = []
+        let currentLevel = 0
+        const TITLE_LEVEL = /^#+/
+        const existingIds = new Map()
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i]
+          if (!line.startsWith('#')) continue
+          const level = line.match(TITLE_LEVEL)[0].length
+
+          // remove extra levels
+          if (level <= currentLevel) {
+            titleStack.splice(level - 1)
+          }
+          // add the current title
+          titleStack.push(line.slice(level).trim())
+          currentLevel = level
+
+          // no need to add ids to h1
+          if (level < 2) continue
+
+          // ignore the root level (h1) to match the sidebar
+          const slugifiedTitle = slugify(titleStack.slice(1).join('-'))
+          let id
+          if (existingIds.has(slugifiedTitle)) {
+            const current = existingIds.get(slugifiedTitle)
+            existingIds.set(slugifiedTitle, current + 1)
+            id = ` %{#${slugifiedTitle}_${current + 1}}%`
+          } else {
+            existingIds.set(slugifiedTitle, 0)
+            id = ` %{#${slugifiedTitle}}%`
+          }
+          const newLine = line + id
+          lines.splice(i, 1, newLine)
+        }
+
+        page.contents = lines.join('\n')
+      }
     }
   )
 
